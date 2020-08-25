8f1d554fb7ce2ec9ef34f01a2c74b89d5b519527
PM-Git <50369307+pm-git@users.noreply.github.com>
2020-08-25 10:42:22 +0530

    Added template project

---
+++ MVVM Template Project/MVVM Template Project/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  MVVM Template Project
+//
+//  Created by Manish on 25/08/20.
+//  Copyright © 2020 Manish. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
